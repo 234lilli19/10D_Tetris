3a45b0234341ea6a15e7d12c4b7a0c6e94c4c9cb
234lilli19 <lilliwintermayr@gmail.com>
2021-05-18 12:40:50 +0200

    Stein Orange Ricky erstellt.

--- ORANGE_RICKY.java
+++ ORANGE_RICKY.java
@@ -1,20 +1,99 @@
 
-/**
- * Beschreiben Sie hier die Klasse ORANGE_RICKY.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
+import java.awt.*;
+import javax. swing. *;
+
  class ORANGE_RICKY extends SPIELSTEIN
 {
+    private JFrame fenster;
     
-    /**
-     * Konstruktor für Objekte der Klasse ORANGE_RICKY
-     */
-     ORANGE_RICKY()
+    private static final int groesse = SPIELFELD.RasterGroesseGeben ();
+
+    private static int radius = groesse / 4;
+    private int x;
+    private int y;
+    private char ausrichtung;
+    private JComponent anzeige;
+    
+    ORANGE_RICKY()
     {
-       
+        fenster = SPIELFELD. FensterGeben ();
+        anzeige = new JComponent () {
+            // Stellt den 1. Stein von Orange Ricky auf dem Fenster dar.
+            public void paintComponent (Graphics a)
+            {
+                a. clearRect (0, 0, groesse, groesse);
+                a. setColor (new Color (255, 127, 0));
+                a. fillRect (0, 0, groesse, groesse);
+                a. setColor (Color. black);
+                a. drawRect (0, 0, groesse, groesse);
+            }
+
+        };
+        anzeige. setSize (groesse, groesse);
+        fenster. add (anzeige, 0);
+        PositionSetzen (0, 1);
+        
+        
+        anzeige = new JComponent () {
+            // Stellt den 2. Stein von Orange Ricky auf dem Fenster dar.
+            public void paintComponent (Graphics a)
+            {
+                a. clearRect (0, 0, groesse, groesse);
+                a. setColor (new Color (255, 127, 0));
+                a. fillRect (0, 0, groesse, groesse);
+                a. setColor (Color. black);
+                a. drawRect (0, 0, groesse , groesse );
+            }
+
+        };
+        anzeige. setSize (groesse, groesse);
+        fenster. add (anzeige, 0);
+        PositionSetzen (1, 1);
+        
+        
+        anzeige = new JComponent () {
+            // Stellt den 3. Stein von Orange Ricky auf dem Fenster dar.
+            public void paintComponent (Graphics a)
+            {
+                a. clearRect (0, 0, groesse, groesse);
+                a. setColor (new Color (255, 127, 0));
+                a. fillRect (0, 0, groesse, groesse);
+                a. setColor (Color. black);
+                a. drawRect (0, 0, groesse , groesse );
+            }
+
+        };
+        
+        anzeige. setSize (groesse, groesse);
+        fenster. add (anzeige, 0);
+        PositionSetzen (2, 1);
+        
+        
+        anzeige = new JComponent () {
+            // Stellt den 4. Stein von Orange Ricky auf dem Fenster dar.
+            public void paintComponent (Graphics a)
+            {
+                a. clearRect (0, 0, groesse, groesse);
+                a. setColor (new Color (255, 127, 0));
+                a. fillRect (0, 0, groesse, groesse);
+                a. setColor (Color. black);
+                a. drawRect (0, 0, groesse , groesse );
+            }
+
+        };
+        anzeige. setSize (groesse, groesse);
+        fenster. add (anzeige, 0);
+        PositionSetzen (2, 0);
+
     }
 
     
+    public void PositionSetzen (int x, int y)
+    {
+       this. x = x;
+       this. y = y;
+       anzeige. setLocation (SPIELFELD. FensterBreiteGeben () / 2 + x * groesse, SPIELFELD. FensterHoeheGeben () / 2 + y * groesse);
+    }
+
+   
 }
