68246f778cd83ff0b2c1e99ac332f9f6a6ce4b1b
antonia5555 <antonia.mauckner@>
2021-05-19 09:40:27 +0200

    Stein Blue Ricky erstellt

--- BLUE_RICKY.java
+++ BLUE_RICKY.java
@@ -5,15 +5,106 @@
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-class BLUE_RICKY extends SPIELSTEIN
+import java.awt.*;
+import javax. swing. *;
+
+class BLUE_RICKY extends SPIELSTEIN 
+
+
 {
-   
-    /**
-     * Konstruktor für Objekte der Klasse BLUE_RICKY
-     */
-     BLUE_RICKY()
+    private JFrame fenster;
+    
+    private static final int groesse = SPIELFELD.RasterGroesseGeben ();
+
+    private static int radius = groesse / 4;
+    private int x;
+    private int y;
+    private char ausrichtung;
+    private JComponent anzeige;
+    
+    BLUE_RICKY()
     {
-       
+        fenster = SPIELFELD. FensterGeben ();
+        anzeige = new JComponent () {
+            // Stellt den 1. Stein von Blue Ricky auf dem Fenster dar.
+            public void paintComponent (Graphics a)
+            {
+                a. clearRect (0, 0, groesse, groesse);
+                a. setColor (new Color (0, 0, 205));
+                a. fillRect (0, 0, groesse, groesse);
+                a. setColor (Color. black);
+                a. drawRect (0, 0, groesse, groesse);
+            }
+
+        };
+        anzeige. setSize (groesse, groesse);
+        fenster. add (anzeige, 0);
+        PositionSetzen (0, 1);
+        
+        
+        anzeige = new JComponent () {
+            // Stellt den 2. Stein von Blue Ricky auf dem Fenster dar.
+            public void paintComponent (Graphics a)
+            {
+                a. clearRect (0, 0, groesse, groesse);
+                a. setColor (new Color (0, 0, 205));
+                a. fillRect (0, 0, groesse, groesse);
+                a. setColor (Color. black);
+                a. drawRect (0, 0, groesse , groesse );
+            }
+
+        };
+        anzeige. setSize (groesse, groesse);
+        fenster. add (anzeige, 0);
+        PositionSetzen (1, 1);
+        
+        
+        anzeige = new JComponent () {
+            // Stellt den 3. Stein von Blue Ricky auf dem Fenster dar.
+            public void paintComponent (Graphics a)
+            {
+                a. clearRect (0, 0, groesse, groesse);
+                a. setColor (new Color (0, 0, 205));
+                a. fillRect (0, 0, groesse, groesse);
+                a. setColor (Color. black);
+                a. drawRect (0, 0, groesse , groesse );
+            }
+
+        };
+        
+        anzeige. setSize (groesse, groesse);
+        fenster. add (anzeige, 0);
+        PositionSetzen (2, 1);
+        
+        
+        anzeige = new JComponent () {
+            // Stellt den 4. Stein von Blue Ricky auf dem Fenster dar.
+            public void paintComponent (Graphics a)
+            {
+                a. clearRect (0, 0, groesse, groesse);
+                a. setColor (new Color (0, 0, 205));
+                a. fillRect (0, 0, groesse, groesse);
+                a. setColor (Color. black);
+                a. drawRect (0, 0, groesse , groesse );
+            }
+
+        };
+        anzeige. setSize (groesse, groesse);
+        fenster. add (anzeige, 0);
+        PositionSetzen (0, 0);
+
     }
 
+    
+    public void PositionSetzen (int x, int y)
+    {
+       this. x = x;
+       this. y = y;
+       anzeige. setLocation (SPIELFELD. FensterBreiteGeben () / 2 + x * groesse, SPIELFELD. FensterHoeheGeben () / 2 + y * groesse);
+    }
+
+   
 }
+
+
+   
